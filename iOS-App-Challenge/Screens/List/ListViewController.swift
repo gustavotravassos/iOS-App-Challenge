4c09e266ec0fc6b7eb139456c6d2f35dbc0cee82
gustavotravassos <gutravassos@gmail.com>
2020-10-17 14:42:52 -0300

    List layout + TableView initial functions

--- iOS-App-Challenge/Screens/List/ListViewController.swift
+++ iOS-App-Challenge/Screens/List/ListViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ListViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var mainTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - TableView Delegate
+extension ListViewController: UITableViewDelegate {}
+
+
+// MARK: - TableView DataSource
+extension ListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
 }
